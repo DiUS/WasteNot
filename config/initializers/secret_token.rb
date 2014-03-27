819c11b40b277f4442fc682648a91701f686bc97
danielbaker <dbaker@dius.com.au>
2014-03-28 09:34:19 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WasteNot::Application.config.secret_key_base = '7a41f7a3d635819b51259af09f9d315e358c6bea0e70c58d3b18b2b3904bfcfa9161796f77aec6790d95ea17415ec3f1943a4cf8aa2bf8fcafa98c35b93bb3b4'
