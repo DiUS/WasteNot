0b44d45f323e9c6f1b3ce8aa8efe4bb9b376a25d
Sonny Mai <sonny@lowfiredanger.com.au>
2014-03-28 10:11:02 +1100

    About page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ WasteNot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'offers#index'
+  get 'about' => 'pages#about', as: :pages_about
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
