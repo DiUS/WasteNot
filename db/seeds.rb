76e43564c40691602afa8ee8aa127dd7c78dfc28
danielbaker <dbaker@dius.com.au>
2014-03-28 13:21:20 +1100

    adding seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+categories = [
+  ["Clothes", "Clothes description"],
+  ["Food & Beverage", "foodies"]
+]
+charities = [
+  ["St Vinnies", "Where all the cool kids hang", "www.vinnies.org.au‎", "Vincent", "0412345678", "admin@vinnies.com.au"]
+]
+retailers = [
+  ["Myer", "Myer is a big retailer with a long bio", "www.myer.com.au", "Bernie Brookes", "0412345678", "contact@myer.com.au"]
+]
+offers = [
+  ["Bread", "Fresh bread which we dont need", "Glebe, Sydney", nil, 1, 1]
+]
+
+categories.each do |name, description|
+  Category.create("name" => name, "description" => description)
+end
+
+charities.each do |name, bio, webpage, contact_name, contact_phone, contact_email|
+  Charity.create("name" => name, "bio" => bio, "webpage" => webpage, "contact_name" => contact_name, "contact_phone" => contact_phone, "contact_email" => contact_email)
+end
+
+retailers.each do |name, bio, web_page, contact_name, contact_phone, contact_email|
+  Retailer.create("name" => name, "bio" => bio, "web_page" => web_page, "contact_name" => contact_name, "contact_phone" => contact_phone, "contact_email" => contact_email)
+end
+
+offers.each do |title, description, location, charity_id, retailer_id, category_id|
+  Offer.create("title" => title, "description" => description, "location" => location, "charity_id" => charity_id, "retailer_id" => retailer_id, "category_id" => category_id)
+end
