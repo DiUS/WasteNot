0d7dc63814c9a2599544b6b19ecf30565c35310d
Sonny Mai <sonny@lowfiredanger.com.au>
2014-03-28 12:55:04 +1100

    Remove turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap
 //= require imagesloaded.pkgd
 //= require jquery.masonry
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,13 @@ class SessionsController < ApplicationController
   def set_charity
     session[:c_org] = Charity.find(params[:id])
     session[:r_org] = nil
-    redirect_to :back
+    redirect_to offers_path
   end
 
   def set_retailer
     session[:r_org] = Retailer.find(params[:id])
     session[:c_org] = nil
-    redirect_to :back
+    redirect_to offers_path
   end
 
   def unset_org
